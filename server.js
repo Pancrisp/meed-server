57b92de4770f590072e15b56464ccf9b2a464da3
Matthew Box <s3559061@student.rmit.edu.au>
2018-04-09 18:36:04 +1000

    Price fetching appears to be working
    Asynchronous stuff is tricky...
    Decided to use semicolons as well

--- server.js
+++ server.js
@@ -1,65 +1,87 @@
 const http = require('http');
 const app = require('./app');
-const axios = require('axios')
-const mongoose = require('mongoose')
+const axios = require('axios');
+const mongoose = require('mongoose');
 const fs = require('fs');
 
-const port = process.env.PORT || 5000
-const server = http.createServer(app)
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
 
-const SharePrice = require('./api/models/sharePrice')
+const SharePrice = require('./api/models/sharePrice');
 
-const lines = fs.readFileSync('symbols.csv').toString().split('\n')
-var symbols = []
-for (var i in lines) {
-  var symbol = lines[i].split(',')[0]
+const lines = fs.readFileSync('symbols.csv').toString().split('\n');
+var symbols = [];
+lines.forEach((line) => {
+  var symbol = line.split(',')[0];
   if (symbol) {
-    symbols.push(symbol)
+    symbols.push(symbol);
   }
-}
-console.log('Loaded ' + symbols.length + ' symbols.')
+});
+console.log('Loaded ' + symbols.length + ' symbols.');
 
 function pad2digits(num) {
   if (num < 10) {
-    return '0' + num
+    return '0' + num;
   } else {
-    return num
+    return num;
   }
 }
 
-const apikey = '6GOVBYU35WIUMU2X'
-symbol = 'TLS'
-const url = 'https://www.alphavantage.co/'
-  + 'query?function=TIME_SERIES_DAILY&symbol='
-  + symbol + '.AX&apikey=' + apikey
+const now = new Date();
+const year = now.getFullYear();
+const month = pad2digits(now.getMonth() + 1);
+const day = pad2digits(now.getDate());
+const datestr = year + '-' + month + '-' + day;
+console.log('Today: ' + datestr);
 
-const now = new Date()
-const year = now.getFullYear()
-const month = pad2digits(now.getMonth() + 1)
-const day = pad2digits(now.getDate())
-const datestr = year + '-' + month + '-' + day
-console.log('Today: ' + datestr)
+const apikey = '6GOVBYU35WIUMU2X';
 
-console.log('Fetching prices...')
-axios.get(url)
-  .then((res) => {
-    console.log('Prices fetched.')
-    //console.log(res.data)
-    price = res.data['Time Series (Daily)'][datestr]['1. open']
-    console.log("Today's price:")
-    console.log(price)
-    sharePrice = new SharePrice({
-      _id: new mongoose.Types.ObjectId(),
-      symbol: symbol,
-      price: price,
-      date: now
-    })
-    //sharePrice.save()
-    console.log('Share Price saved to database:\n' + sharePrice)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
+// Recursively fetch the prices
+// (So that we get each price one at a time)
+console.log('Fetching prices...');
+fetchPrices(0);
+// Start listening
 server.listen(port);
 console.log('API server started on port ' + port);
+
+function fetchPrices(index) {
+  symbol = symbols[index];
+  const url = 'https://www.alphavantage.co/'
+    + 'query?function=TIME_SERIES_DAILY&symbol='
+    + symbol + '.AX&apikey=' + apikey;
+
+  axios.get(url)
+    .then((res) => {
+      if (!res.data) {
+        throw "No data in response!";
+      }
+      const price = res.data['Time Series (Daily)'][datestr]['1. open'];
+      SharePrice.findOne({symbol: symbol}, (err, sharePrice) => {
+        if (err) throw err;
+        if (sharePrice) {
+          sharePrice.price = price;
+          sharePrice.date = now;
+          console.log('Updated price ' + symbol + ' ' + price);
+        } else {
+          sharePrice = new SharePrice({
+            _id: new mongoose.Types.ObjectId(),
+            symbol: symbol,
+            price: price,
+            date: now
+          });
+          console.log('Added new price ' + symbol + ' ' + price);
+        }
+        sharePrice.save();
+        if (index + 1 == symbols.length) {
+          console.log('All ' + (index + 1) + ' prices fetched.');
+        } else {
+          fetchPrices(index + 1);
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// vi: sw=2
