2a9ddc2ee5bd965d69b6edf61a73fb30f76ced92
Matthew Box <mattbox87@gmail.com>
2018-04-09 16:15:24 +1000

    WIP: loading share price symbols

--- server.js
+++ server.js
@@ -1,26 +1,61 @@
 const http = require('http');
 const app = require('./app');
 const axios = require('axios')
+const mongoose = require('mongoose')
+const fs = require('fs');
 
 const port = process.env.PORT || 5000
 const server = http.createServer(app)
 
 const SharePrice = require('./api/models/sharePrice')
 
-console.log('Fetching prices...')
+const lines = fs.readFileSync('symbols.csv').toString().split('\n')
+var symbols = []
+for (var i in lines) {
+  var symbol = lines[i].split(',')[0]
+  if (symbol) {
+    symbols.push(symbol)
+  }
+}
+console.log('Loaded ' + symbols.length + ' symbols.')
+
+function pad2digits(num) {
+  if (num < 10) {
+    return '0' + num
+  } else {
+    return num
+  }
+}
 
 const apikey = '6GOVBYU35WIUMU2X'
-const symbol = 'TLS'
+symbol = 'TLS'
 const url = 'https://www.alphavantage.co/'
   + 'query?function=TIME_SERIES_DAILY&symbol='
   + symbol + '.AX&apikey=' + apikey
 
+const now = new Date()
+const year = now.getFullYear()
+const month = pad2digits(now.getMonth() + 1)
+const day = pad2digits(now.getDate())
+const datestr = year + '-' + month + '-' + day
+console.log('Today: ' + datestr)
+
+console.log('Fetching prices...')
 axios.get(url)
   .then((res) => {
     console.log('Prices fetched.')
-    console.log(res.data)
+    //console.log(res.data)
+    price = res.data['Time Series (Daily)'][datestr]['1. open']
     console.log("Today's price:")
-    console.log(res.data['Time Series (Daily)']['2018-04-09'])
+    console.log(price)
+    sharePrice = new SharePrice({
+      _id: new mongoose.Types.ObjectId(),
+      symbol: symbol,
+      price: price,
+      date: now
+    })
+    //sharePrice.save()
+    console.log('Share Price saved to database:\n' + sharePrice)
   })
   .catch((err) => {
     console.log(err)
