4408fe2515d986e47003a2de266639ac08424a17
Diane Loi <dianeloi_8@hotmail.com>
2018-04-23 20:44:04 +1000

    Find top 10 accounts

--- api/controllers/leaderboard.js
+++ api/controllers/leaderboard.js
@@ -3,5 +3,57 @@ const User = require('../models/user')
 const Account = require('../models/account')
 
 exports.view = (req, res, next) => {
-//find top 10 accounts
+  //get user
+  //get accounts in users that have transactions
+  //find networth
+  //rank them in order in array
+
+  // let leaderboard = [];
+  //
+  // User.find().exec((err,users) => {
+  //   users.forEach(function(user){
+  //     // console.log(user);
+  //
+  //     user.accounts.forEach(function(id) {
+  //       for(let i=0;i<user.accounts.length;i++) {
+  //         Account.findById(id).exec(function(err,account) {
+  //           // console.log(account.networth);
+  //           // console.log(user.name);
+  //           leaderboard.push({
+  //             name: user.name,
+  //             account: i+1,
+  //             networth: account.networth
+  //           });
+  //         });
+  //       }
+  //     });
+  //   });
+  // });
+  // console.log(leaderboard);
+  // res.json(leaderboard);
+
+
+
+  let userArray = [];
+  let accountArray =[];
+
+  User.find().exec((err,users)
+  //get accounts
+
+
+  //get users
 };
+
+
+// exports.viewAll = (req, res, next) => {
+//   Share.find().exec((err, shares) => {
+//     if (!shares) {
+//       return res.status(404).json({
+//         message: 'No shares!'
+//       });
+//     } else {
+//       res.json(shares)
+//       console.log('Returned query for all shares');
+//     }
+//   });
+// };
