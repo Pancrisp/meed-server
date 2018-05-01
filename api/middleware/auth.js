cde2c8525e193b31e9e4d6fbb8291c1b73921244
Ervin Chua <ervinchua@gmail.com>
2018-05-01 11:09:49 +1000

    Add missing next()

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -2,8 +2,9 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]
-    const decoded = jwt.verify(token, process.env.JWT_KEY)
+    // const token = req.headers.authorization.split(" ")[1]
+    req.payload = jwt.verify(req.body.token, process.env.JWT_KEY)
+    next()
   } catch (err) {
     return res.status(401).json({
       message: "Authentication failed"
