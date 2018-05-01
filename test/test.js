70f95102a5ede684c3e66fba001541974315350a
Matthew Box <s3559061@student.rmit.edu.au>
2018-05-01 17:54:17 +1000

    Back to basics with testing
    It turns out we can depend on mocha performing the tests
    sequentially so it is not necessary to combine creating and deleting
    the user as previously thought

--- test/test.js
+++ test/test.js
@@ -4,47 +4,34 @@ const axios = require('axios');
 const localUrl = 'http://localhost:5000';
 const remoteUrl = 'https://fierce-lake-99257.herokuapp.com';
 
-function createUser(host) {
-  const url = host + '/users/signup';
-  return axios.post(url, {
-    name: 'test',
-    email: 'test',
-    password: 'test'
-  });
-}
-
-function deleteUser(host, userId) {
-  const url = host + '/users/' + userId;
-  return axios.delete(url);
-}
+let testUserId = 0;
 
 describe('Users', function() {
-  describe('signup and delete: local', function() {
-    it('should create a new user and then delete it', function(done) {
+  describe('signup', function() {
+    it('should create a new user', function(done) {
       this.timeout(10000);
-      createUser(localUrl)
-        .then(function(res) {
-          deleteUser(localUrl, res.data.user._id)
-            .then(function(res) {
-              if (res.status == 200) {
-                done();
-              }
-            });
-        });
+      const url = localUrl + '/users/signup';
+      axios.post(url, {
+        name: 'test',
+        email: 'test',
+        password: 'test'
+      }).then(function(res) {
+        if (res.status == 201) {
+          testUserId = res.data.user._id;
+          done();
+        }
+      });
     });
   });
-  describe('signup and delete: remote', function() {
-    it('should create a new user and then delete it', function(done) {
+  describe('delete', function() {
+    it('should delete the new user', function(done) {
       this.timeout(10000);
-      createUser(remoteUrl)
-        .then(function(res) {
-          deleteUser(remoteUrl, res.data.user._id)
-            .then(function(res) {
-              if (res.status == 200) {
-                done();
-              }
-            });
-        });
+      const url = localUrl + '/users/' + testUserId;
+      axios.delete(url).then(function(res) {
+        if (res.status == 200) {
+          done();
+        }
+      });
     });
   });
 });
