6415c88323136f8b410e7e44e176e37f00373fc9
Matthew Box <s3559061@student.rmit.edu.au>
2018-05-01 16:18:59 +1000

    Basic testing
    Running npm test will now check that a user can be created and
    deleted both on a locally running server as well as on heroku

--- test/test.js
+++ test/test.js
@@ -2,18 +2,51 @@ const assert = require('assert');
 const axios = require('axios');
 
 const localUrl = 'http://localhost:5000';
+const remoteUrl = 'https://fierce-lake-99257.herokuapp.com';
+
+function createUser(host) {
+  const url = host + '/users/signup';
+  return axios.post(url, {
+    name: 'test',
+    email: 'test',
+    password: 'test'
+  });
+}
+
+function deleteUser(host, userId) {
+  const url = host + '/users/' + userId;
+  return axios.delete(url);
+}
 
 describe('Users', function() {
-    describe('signup-local', function() {
-        it('should create a new user', function(done) {
-            const url = localUrl + '/users/signup';
-            axios.post(url, {
-                name: 'test',
-                email: 'test',
-                password: 'test'
-            }).then(function(res) {
-                console.log(res);
+  describe('signup and delete: local', function() {
+    it('should create a new user and then delete it', function(done) {
+      this.timeout(10000);
+      createUser(localUrl)
+        .then(function(res) {
+          deleteUser(localUrl, res.data.user._id)
+            .then(function(res) {
+              if (res.status == 200) {
+                done();
+              }
             });
         });
     });
+  });
+  describe('signup and delete: remote', function() {
+    it('should create a new user and then delete it', function(done) {
+      this.timeout(10000);
+      createUser(remoteUrl)
+        .then(function(res) {
+          deleteUser(remoteUrl, res.data.user._id)
+            .then(function(res) {
+              if (res.status == 200) {
+                done();
+              }
+            });
+        });
+    });
+  });
 });
+
+// vi: sw=2
