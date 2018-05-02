e36968f2df4b5cc44c009b51fe8425b348885521
Diane Loi <dianeloi_8@hotmail.com>
2018-05-02 16:45:54 +1000

    Reset Password
    Now you can reset your password
    Need to work on testing and expiring the token

--- api/controllers/passwordReset.js
+++ api/controllers/passwordReset.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const env = require('dotenv').config()
 
 const User = require('../models/user')
 const PasswordReset = require('../models/passwordReset')
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.sendEmail = (req, res, next) => {
   if (!req.body.email) {
@@ -18,6 +22,9 @@ exports.sendEmail = (req, res, next) => {
         message: 'No user with that email'
       });
     }
+    PasswordReset.remove({userEmail: userEmail}, function(err) {
+      if (err) return handleError(err);
+    });
     const token = jwt.sign(userEmail, process.env.JWT_SECRET_KEY);
     const reset = new PasswordReset({
       _id: new mongoose.Types.ObjectId(),
@@ -29,11 +36,53 @@ exports.sendEmail = (req, res, next) => {
       const resetUrl = 'https://www.frontend.com/password_reset?email='
         + userEmail + '&token=' + token;
       // email the user and respond OK
+      const msg = {
+        to: userEmail,
+        from: 'noreply@meed.com',
+        subject: 'Password Reset',
+        text: 'Please click on the following link to reset your password: ' +resetUrl,
+      };
+      sgMail.send(msg);
+      res.json({
+        message: "Email sent"
+      });
     });
   });
 };
 
-exports.resetPassword = (req, res, next) => {
+exports.resetPassword = async (req, res, next) => {
+  resetRecord = await PasswordReset.findOne({userEmail:req.body.email});
+
+  if(!resetRecord) {
+    return res.status(404).json({
+      message: 'No password reset request for this user'
+    });
+  }
+
+  //check token against database
+  if(resetRecord.token != req.body.token) {
+    return res.status(404).json({
+      message: 'Token does not match'
+    });
+  }
+
+  //reset password if match
+  userFound = await User.findOne({email:req.body.email});
+
+  if(!userFound) {
+    return res.status(404).json({
+      message: 'User not found'
+    });
+  }
+
+  //find user with email, and hash password and replace it
+  bcrypt.hash(req.body.password, 10, async (err, hash) => {
+    userFound.password = hash;
+    await userFound.save();
+    return res.json({
+      message: "Password Updated"
+    });
+  });
 };
 
 // vi: sw=2
