d294f6eeab777c7077f39b78196144695ec0ae5e
Matthew Box <s3559061@student.rmit.edu.au>
2018-05-04 00:06:46 +1000

    Use of promises has been changed to ES2017 await
    Much easier to read now!
    Tests are failing however as they currently do not authenticate

--- api/controllers/share.js
+++ api/controllers/share.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const Share = require('../models/share')
 
-exports.viewAll = (req, res, next) => {
-  Share.find().exec((err, shares) => {
+exports.viewAll = async (req, res, next) => {
+  try {
+    const shares = await Share.find();
     if (!shares) {
       return res.status(404).json({
         message: 'No shares!'
@@ -11,11 +12,18 @@ exports.viewAll = (req, res, next) => {
       res.json(shares)
       console.log('Returned query for all shares');
     }
-  });
-};
+  } catch (err) {
+    const msg = 'Error thrown when viewing all shares';
+    console.log(msg);
+    return res.status(500).json({
+      message: msg
+    });
+  }
+}
 
-exports.view = (req, res, next) => {
-  Share.findOne({symbol: req.params.symbol}).exec((err, share) => {
+exports.view = async (req, res, next) => {
+  try {
+    const share = await Share.findOne({symbol: req.params.symbol});
     if (!share) {
       return res.status(404).json({
         message: 'No price found for symbol ' + req.params.symbol
@@ -24,5 +32,11 @@ exports.view = (req, res, next) => {
       res.json(share)
       console.log('Returned query for price on symbol ' + share.symbol);
     }
-  });
+  } catch (err) {
+    const msg = 'Error thrown when viewing a share';
+    console.log(msg);
+    return res.status(500).json({
+      message: msg
+    });
+  }
 };
