bb8426a50fcbef088f56d45e6ae8fc7a8d897cc9
Ervin Chua <ervinchua@gmail.com>
2018-05-03 10:13:15 +1000

    Add JWT auth module middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const env = require('dotenv').config()
+const jwt = require('jsonwebtoken')
 
 const userRoutes = require('./api/routes/user')
 const accountRoutes = require('./api/routes/account')
@@ -13,7 +14,6 @@ const leaderboardRoutes = require('./api/routes/leaderboard')
 mongoose.Promise = global.Promise
 // Connect to mLab database server
 mongoose.connect(`mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASS}@ds123619.mlab.com:23619/meed`)
-//mongoose.connect(`mongodb://localhost/meed`)
 
 const app = express()
 
@@ -29,9 +29,9 @@ app.use((req, res, next) => {
   )
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    return res.status(200).json({});
+    return res.status(200).json({})
   }
-  next();
+  next()
 })
 
 // Routes to handle requests, these are our API endpoints
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http');
-const app = require('./app');
+const http = require('http')
+const app = require('./app')
 
-const port = process.env.PORT || 5000;
-const server = http.createServer(app);
+const port = process.env.PORT || 5000
+const server = http.createServer(app)
 
 // Start listening
-server.listen(port);
-console.log('API server started on port ' + port);
+server.listen(port)
+console.log('API server started on port ' + port)
 
 // vi: sw=2
