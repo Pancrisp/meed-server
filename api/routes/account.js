225b15bc889c37d16fd88f2319e4794f769f398e
Ervin Chua <ervinchua@gmail.com>
2018-05-07 18:05:25 +1000

    Temporarily remove route guards

--- api/routes/account.js
+++ api/routes/account.js
@@ -4,9 +4,9 @@ const isAuthenticated = require('../middleware/auth.js')
 
 const AccountController = require('../controllers/account')
 
-router.post('/', isAuthenticated, AccountController.createAccount)
-router.post('/buy', isAuthenticated, AccountController.buy)
-router.post('/sell', isAuthenticated, AccountController.sell)
+router.post('/', AccountController.createAccount)
+router.post('/buy', AccountController.buy)
+router.post('/sell', AccountController.sell)
 router.get('/:accountId', AccountController.view)
 router.delete('/:accountId', AccountController.delete)
 
